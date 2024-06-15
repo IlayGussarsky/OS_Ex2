f0a102abe9d76ad008f93d3ba5c6dd3c69e4c449
TLP-242 <ilay.gussarsky.44@gmail.com>
2024-06-15 16:48:09 +0300

    Made whatsapptests run

--- NewTests/WhatsappTests/test4.cpp
+++ NewTests/WhatsappTests/test4.cpp
@@ -1,4 +1,4 @@
-#include "uthreads.h"
+#include "../../uthreads.h"
 #include "stdio.h"
 #include <signal.h>
 #include <unistd.h>
@@ -6,17 +6,24 @@
 void g()
 {
   printf ("%d ", uthread_get_tid());
+  fflush(stdout);
+
   uthread_sleep (1);
   printf ("%d ", uthread_get_tid());
+  fflush(stdout);
   uthread_terminate (1);
 }
 
 void f()
 {
   printf ("%d ", uthread_get_tid());
+  fflush(stdout);
+
   uthread_block (1);
   uthread_block (2);
   printf ("%d ", uthread_get_tid());
+  fflush(stdout);
+
   uthread_resume (1);
   uthread_terminate(uthread_get_tid());
 }
@@ -28,9 +35,13 @@ int main(int argc, char **argv)
   uthread_spawn (f);
   kill(getpid(),SIGVTALRM);
   printf ("%d ", uthread_get_tid());
+  fflush(stdout);
+
   uthread_resume (2);
   kill(getpid(),SIGVTALRM);
   printf ("%d ", uthread_get_tid());
+  fflush(stdout);
+
   kill(getpid(),SIGVTALRM);
   printf ("\nYou should see: 1 2 0 2 0 1\n");
   uthread_terminate(0);
